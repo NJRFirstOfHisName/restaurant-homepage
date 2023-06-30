631fb2f46a384e3d73dff645427311dc0362fe91
NJRFirstOfHisName <130861854+NJRFirstOfHisName@users.noreply.github.com>
2023-06-30 11:55:24 -0600

    Implement print.js as module to print the webpage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import print from './print.js';
+import printScreen from './print.js';
 import './style.css'
 import hImage from './headerCrop.jpg'
 
-function component() {
-    const element = document.createElement('div');
-
-    const headerImage = new Image();
-    headerImage.src = hImage;
-    element.appendChild(headerImage);
-
-    const headerText = document.createElement("p");
-
-    return element;
-}
-
-document.body.appendChild(component());
+printScreen();
--- src/print.js
+++ src/print.js
@@ -1,5 +1,22 @@
-export default function print() {
-    const headerImage = document.createElement('img');
-    const headerText = document.createElement('h2');
+import hImage from './headerCrop.jpg'
+
+
+export default function printScreen() {
+    const element = document.createElement('div');
+
+    const headerImage = new Image();
+    headerImage.src = hImage;
+    element.appendChild(headerImage);
+
+    const header = document.createElement('h2');
+    const headerText = document.createTextNode("Tastee Boigahs");
+    header.appendChild(headerText);
+    element.appendChild(header);
+
     const promo = document.createElement('p');
+    const promoText = document.createTextNode("Yum yum yum yum")
+    promo.appendChild(promoText);
+    element.appendChild(promo);
+
+    document.body.appendChild(element);
 }
