a75eaedfd911bb2c9b7d3de19d146da44b8a1446
NJRFirstOfHisName <130861854+NJRFirstOfHisName@users.noreply.github.com>
2023-06-30 11:24:37 -0600

    Implement webpack to show a basic display

---
+++ dist/print.bundle.js
@@ -0,0 +1,28 @@
+"use strict";
+(self["webpackChunkrestaurant_homepage"] = self["webpackChunkrestaurant_homepage"] || []).push([["print"],{
+
+/***/ "./src/print.js":
+/*!**********************!*\
+  !*** ./src/print.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ print)
+/* harmony export */ });
+function print() {
+    const headerImage = document.createElement('img');
+    const headerText = document.createElement('h2');
+    const promo = document.createElement('p');
+}
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/print.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHJpbnQuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBZTtBQUNmO0FBQ0E7QUFDQTtBQUNBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vcmVzdGF1cmFudC1ob21lcGFnZS8uL3NyYy9wcmludC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCBmdW5jdGlvbiBwcmludCgpIHtcclxuICAgIGNvbnN0IGhlYWRlckltYWdlID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnaW1nJyk7XHJcbiAgICBjb25zdCBoZWFkZXJUZXh0ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnaDInKTtcclxuICAgIGNvbnN0IHByb21vID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgncCcpO1xyXG59Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/print.js
@@ -0,0 +1,5 @@
+export default function print() {
+    const headerImage = document.createElement('img');
+    const headerText = document.createElement('h2');
+    const promo = document.createElement('p');
+}
